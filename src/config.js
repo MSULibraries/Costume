b7ceffd34c9f75cf696f95dc299e2e8634e55942
Justin E. Samuels <js1599.msu@gmail.com>
2016-09-16 17:04:19 -0500

    initial commit of boiler

--- src/config.js
+++ src/config.js
@@ -15,7 +15,6 @@ module.exports = Object.assign({
   apiHost: process.env.APIHOST || 'localhost',
   apiPort: process.env.APIPORT,
   app: {
-    title: 'React Redux Example',
     description: 'All the modern best practices in one example.',
     head: {
       titleTemplate: 'React Redux Example: %s',
