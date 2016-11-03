c92e24cacbbc9ec34a08f749e8156bfec0892d7a
Justin E. Samuels <js1599@msstate.edu>
2016-11-03 17:10:01 -0500

    user interface started. Adding Authentication

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,19 +1,27 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router';
 import Helmet from 'react-helmet';
 
-
 export default class Home extends Component {
+    constructor(props) {
+    super(props);
+    this.click = this.click.bind(this);
+    this.state = {user: [], name: ''};
+  }
+  click() {
+    console.log('user verified');  
+ }
   render() {
     const styles = require('./Home.scss');
     return (
       <div className={styles.home}>
         <Helmet title="Home"/>
-        <h1> Hello World Test Home I am working Tfeslt Again Again</h1>
-        <button className = "btn btn-success">Go Home Roger</button>
-        <button className = "btn btn-danger">Test ME </button>
-        <Link to="/Help">Help</Link>
+        <h1>Hello World</h1>
+        <input type = "text" /> 
+        &nbsp;&nbsp;
+        <input type = "text" /> &nbsp; &nbsp;
+        <button onClick = {this.click} className = "btn btn-success">Login</button>
       </div>
     );
   }
 }
+
